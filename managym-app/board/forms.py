c28ae1e0470380986ec67233dbe48c266baeb2dc
Boubsinastra <boos.baptiste1@gmail.com>
2020-08-01 10:51:57 -0400

    Add new target for an athlete

---
+++ managym-app/board/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+
+from .models import Event
+
+class NewTargetForm(forms.Form):
+    """Form representing all event without target"""
+    event = forms.ModelChoiceField(queryset=None)
+
+    def __init__(self, athlete_id, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['event'].queryset = Event.objects.exclude(targetresult__athlete__id=athlete_id).distinct()
+        
